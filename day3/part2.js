5ff0c8482a8e4f1ca1c0f6c1b0b71dd6e538681e
bravint <bravint@gmail.com>
2021-12-03 14:25:57 +0000

    clean up code

--- day3/part2.js
+++ day3/part2.js
@@ -17,22 +17,21 @@ fs.readFile('./input.txt', 'utf8' , (err, data) => {
 let ratings = 0;
 
 function rating (data, index, type) {
-    let newData = [...data];
     let arrOne =[];
     let arrZero = [];
 
-    for (let i=0; i < newData.length; i++) {
-        if (newData.length > 1) {
-            if (parseInt(newData[i].charAt(index)) === 1) {
-                arrOne.push(newData[i]);    
-                } else if (parseInt(newData[i].charAt(index)) === 0) {
-                arrZero.push(newData[i]);
+    for (let i=0; i < data.length; i++) {
+        if (data.length > 1) {
+            if (parseInt(data[i].charAt(index)) === 1) {
+                arrOne.push(data[i]);    
+                } else if (parseInt(data[i].charAt(index)) === 0) {
+                arrZero.push(data[i]);
             }  
         } else {
-            return ratings = (parseInt(newData[0], 2));
+            return ratings = (parseInt(data[0], 2));
         } 
     } 
-    
+
     if (type == 'o2') {
         arrOne.length >= arrZero.length ? rating (arrOne, index+1, 'o2') : rating (arrZero, index+1, 'o2');
     } else {
